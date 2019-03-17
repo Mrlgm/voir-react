6a005fa3691dc5d0c9308e981c9ef003099acb9b
Mrlgm <877205619@qq.com>
2019-03-17 13:53:25 +0800

    webpack可以打包了

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'voir-react',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+}
