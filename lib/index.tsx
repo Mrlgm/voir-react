d85f1f2dcdd594ae645df86195c127bf75fbebd0
Mrlgm <877205619@qq.com>
2019-03-31 11:31:22 +0800

    打包发布到npm

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from './button/button';
-import './style/index.scss';
-
-
-//不是html
-ReactDOM.render(<Button>你好</Button>, document.querySelector('#root'));
+export {default as Icon} from './icon/icon';
