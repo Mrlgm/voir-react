0f3d978dbccf90b244b5f7e522263855341d456e
Mrlgm <877205619@qq.com>
2019-04-14 16:25:04 +0800

    给 dialog 添加绑定事件

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -2,9 +2,15 @@ import React, {useState} from 'react';
 import Dialog, {alert} from './dialog';
 
 export default function () {
-    const [visible] = useState(true);
+    const [visible, setVisible] = useState(false);
     return (<div>
-        <Dialog visible={visible}>hi</Dialog>
+        <button onClick={() => {setVisible(!visible);}}>click</button>
+        <Dialog visible={visible} buttons={
+            [
+                <button onClick={() => {setVisible(false);}}>1</button>,
+                <button onClick={() => {setVisible(false);}}>2</button>
+            ]
+        } onClose={() => {setVisible(false);}}>hi</Dialog>
         <div>
             <button onClick={e => alert('hhh')}>hhh</button>
         </div>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,23 +1,35 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, MouseEventHandler, ReactElement} from 'react';
 import ReactDOM from 'react-dom';
-import Button from '../button/button';
 import './style/dialog.scss';
 import createScopedClass from '../classes';
 
 interface Props {
     visible: boolean;
+    buttons: Array<ReactElement>;
+    onClose: MouseEventHandler;
+    closeOnClickMask?: boolean;
 }
 
 const scopedClass = createScopedClass('vi-dialog');
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
+    const onClickClose: MouseEventHandler = (e) => {
+        props.onClose(e);
+    };
+
+    const onClickMask: MouseEventHandler = (e) => {
+        if (props.closeOnClickMask) {
+            props.onClose(e);
+        }
+    };
+
     const x = props.visible ?
         <Fragment>
-            <div className={scopedClass('mask')}>
+            <div className={scopedClass('mask')} onClick={onClickMask}>
 
             </div>
             <div className={scopedClass()}>
-                <div className={scopedClass('close')}>
+                <div className={scopedClass('close')} onClick={onClickClose}>
                     X
                 </div>
                 <header className={scopedClass('header')}>
@@ -27,8 +39,9 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
                     {props.children}
                 </main>
                 <footer className={scopedClass('footer')}>
-                    <Button>ok</Button>
-                    <Button>cancel</Button>
+                    {props.buttons.map((button, index) => {
+                        return React.cloneElement(button, {key: index});
+                    })}
                 </footer>
             </div>
         </Fragment> :
@@ -36,12 +49,22 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
     return ReactDOM.createPortal(x, document.body);
 };
 
+Dialog.defaultProps = {
+    closeOnClickMask: false
+};
+
 const alert = (content: string) => {
-    const component = <Dialog visible={true}>content</Dialog>;
+    const component = <Dialog buttons={[]} visible={true} onClose={() => {
+        ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+        //删除div 使用react的方法，先卸载
+        ReactDOM.unmountComponentAtNode(div);
+        div.remove();
+    }}>{content}</Dialog>;
     const div = document.createElement('div');
     document.body.appendChild(div);
     ReactDOM.render(component, div);
 };
+
 
 export {alert};
 export default Dialog;
