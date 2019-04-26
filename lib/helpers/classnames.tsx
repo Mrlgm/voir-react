facb86c005a0b53fedf713bd36319d039972f9dd
Mrlgm <877205619@qq.com>
2019-04-26 23:14:48 +0800

    修改 createScopedClass 功能

--- lib/helpers/classnames.tsx
+++ lib/helpers/classnames.tsx
@@ -4,9 +4,23 @@ function classNames(...names: (string | undefined)[]) {
 
 export default classNames;
 
+interface ClassToggles {
+    [key: string]: boolean
+}
+
 function createScopedClass(prefix: string) {
-    return function (name?: string) {
-        return [prefix, name].filter(Boolean).join('-');
+    return function (name?: string | ClassToggles) {
+        let result;
+        let name2;
+        if (typeof name === 'string' || name === undefined) {
+            result = [prefix, name].filter(Boolean).join('-');
+        } else {
+            name2 = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0]);
+            result = name2.map(name => {
+                [prefix, name].filter(Boolean).join('-');
+            }).join(' ');
+        }
+        return result;
     };
 }
 
