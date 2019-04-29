c6b90d0a30bf13b2b1f1016f092e60209617f372
Mrlgm <877205619@qq.com>
2019-04-29 22:19:35 +0800

    重构createScopedClass

--- lib/helpers/classnames.tsx
+++ lib/helpers/classnames.tsx
@@ -9,18 +9,16 @@ interface ClassToggles {
 }
 
 function createScopedClass(prefix: string) {
-    return function (name?: string | ClassToggles) {
-        let result;
-        let name2;
-        if (typeof name === 'string' || name === undefined) {
-            result = [prefix, name].filter(Boolean).join('-');
-        } else {
-            name2 = Object.entries(name).filter(kv => kv[1] !== false).map(kv => kv[0]);
-            result = name2.map(name => {
-                return [prefix, name].filter(Boolean).join('-');
-            }).join(' ');
-        }
-        return result;
+    return function (name: string | ClassToggles = '') {
+        const nameObject = typeof name === 'string' ? {[name]: name} : name;
+        return Object
+            .entries(nameObject)
+            .filter(kv => kv[1] !== false)
+            .map(kv => kv[0])
+            .map(name => [prefix, name]
+                .filter(Boolean)
+                .join('-'))
+            .join(' ');
     };
 }
 
