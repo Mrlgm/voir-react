5e9c3651fc9e5a9a94d1e8b171bc91d85c37fd7d
Mrlgm <877205619@qq.com>
2019-04-30 23:48:06 +0800

    完成 layout 组件示例

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -4,48 +4,49 @@ import Footer from './footer';
 import Content from './content';
 import Header from './header';
 import Aside from './aside';
+import './style/layout.example.scss';
 
 export default function () {
     return (
         <div>
             <div>
                 <h1>第一个例子</h1>
-                <Layout style={{height: 500}} className='hi'>
-                    <Header>header</Header>
-                    <Content>content</Content>
-                    <Footer>footer</Footer>
+                <Layout  className='hi'>
+                    <Header className='header'>header</Header>
+                    <Content className='content'>content</Content>
+                    <Footer className='footer'>footer</Footer>
                 </Layout>
             </div>
             <div>
                 <h1>第二个例子</h1>
-                <Layout style={{height: 500}} className='hi'>
-                    <Header>header</Header>
+                <Layout  className='hi'>
+                    <Header className='header'>header</Header>
                     <Layout>
-                        <Aside>aside</Aside>
-                        <Content>content</Content>
+                        <Aside className='aside'>aside</Aside>
+                        <Content className='content'>content</Content>
                     </Layout>
-                    <Footer>footer</Footer>
+                    <Footer className='footer'>footer</Footer>
                 </Layout>
             </div>
             <div>
                 <h1>第三个例子</h1>
-                <Layout style={{height: 500}} className='hi'>
-                    <Header>header</Header>
+                <Layout  className='hi'>
+                    <Header className='header'>header</Header>
                     <Layout>
-                        <Content>content</Content>
-                        <Aside>aside</Aside>
+                        <Content className='content'>content</Content>
+                        <Aside className='aside'>aside</Aside>
                     </Layout>
-                    <Footer>footer</Footer>
+                    <Footer className='footer'>footer</Footer>
                 </Layout>
             </div>
             <div>
                 <h1>第四个例子</h1>
-                <Layout style={{height: 500}} className='hi'>
-                    <Aside>aside</Aside>
+                <Layout  className='hi'>
+                    <Aside className='aside'>aside</Aside>
                     <Layout>
-                        <Header>header</Header>
-                        <Content>content</Content>
-                        <Footer>footer</Footer>
+                        <Header className='header'>header</Header>
+                        <Content className='content'>content</Content>
+                        <Footer className='footer'>footer</Footer>
                     </Layout>
                 </Layout>
             </div>
