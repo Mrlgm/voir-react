88a74272c0279139b637417e28848aef318a7faa
Mrlgm <877205619@qq.com>
2019-05-02 00:22:39 +0800

    完成简陋官网

--- example.tsx
+++ example.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter as Router, Link, Route} from 'react-router-dom';
 import DialogExample from './lib/dialog/dialog.example';
 import IconExample from './lib/icon/icon.example';
 import LayoutExample from './lib/layout/layout.example';
 
 function Example() {
     return (
-        <div>
-            <header>
-                <div className="logo">
-                    VI
-                </div>
-            </header>
-            <aside>
-                <h2>组件</h2>
-                <ul>
-                    <li>Icon</li>
-                    <li>Dialog</li>
-                </ul>
-            </aside>
-            <IconExample/>
-            <DialogExample/>
-            <LayoutExample/>
-        </div>
+        <Router>
+            <div>
+                <header>
+                    <div className="logo">
+                        VI
+                    </div>
+                </header>
+                <aside>
+                    <h2>组件</h2>
+                    <ul>
+                        <li><Link to="/icon">Icon</Link></li>
+                        <li><Link to="/dialog">Dialog</Link></li>
+                        <li><Link to="/layout">Layout</Link></li>
+                    </ul>
+                </aside>
+                <main>
+                    <Route path="/icon" component={IconExample}/>
+                    <Route path="/dialog" component={DialogExample}/>
+                    <Route path="/layout" component={LayoutExample}/>
+                </main>
+            </div>
+        </Router>
     );
 }
 
