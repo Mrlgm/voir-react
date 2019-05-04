bb4c9b3a74481d5c224d33dd87c61868d1471a6f
Mrlgm <877205619@qq.com>
2019-05-04 23:41:13 +0800

    使用自己的轮子布局

--- example.tsx
+++ example.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './example.scss';
 import {HashRouter as Router, Link, Route} from 'react-router-dom';
 import DialogExample from './lib/dialog/dialog.example';
 import IconExample from './lib/icon/icon.example';
 import LayoutExample from './lib/layout/layout.example';
+import {Layout, Aside, Content, Footer, Header} from './lib/layout/layout';
 
 function Example() {
     return (
         <Router>
-            <div>
-                <header>
+            <Layout className='page'>
+                <Header>
                     <div className="logo">
                         VI
                     </div>
-                </header>
-                <aside>
-                    <h2>组件</h2>
-                    <ul>
-                        <li><Link to="/icon">Icon</Link></li>
-                        <li><Link to="/dialog">Dialog</Link></li>
-                        <li><Link to="/layout">Layout</Link></li>
-                    </ul>
-                </aside>
-                <main>
-                    <Route path="/icon" component={IconExample}/>
-                    <Route path="/dialog" component={DialogExample}/>
-                    <Route path="/layout" component={LayoutExample}/>
-                </main>
-            </div>
+                </Header>
+                <Layout>
+                    <Aside>
+                        <h2>组件</h2>
+                        <ul>
+                            <li><Link to="/icon">Icon</Link></li>
+                            <li><Link to="/dialog">Dialog</Link></li>
+                            <li><Link to="/layout">Layout</Link></li>
+                        </ul>
+                    </Aside>
+                    <Content>
+                        <Route path="/icon" component={IconExample}/>
+                        <Route path="/dialog" component={DialogExample}/>
+                        <Route path="/layout" component={LayoutExample}/>
+                    </Content>
+                </Layout>
+                <Footer>footer</Footer>
+            </Layout>
         </Router>
     );
 }
