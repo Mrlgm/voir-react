03c037022748552b84ed454c28c3cdd505269ece
Mrlgm <877205619@qq.com>
2019-12-03 22:03:17 +0800

    实现form组件简单校验

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form, {FormValue} from './form';
 import {useState, Fragment} from 'react';
+import Validator from './validator';
 
 const FormExample: React.FunctionComponent = () => {
     const [formData, setFormData] = useState<FormValue>({
@@ -11,8 +12,13 @@ const FormExample: React.FunctionComponent = () => {
         {name: 'username', label: '用户名', input: {type: 'text'}},
         {name: 'password', label: '密码', input: {type: 'password'}},
     ]);
-    const onSubmit = (e: React.FormEvent) => {
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         console.log(formData);
+        const rules = [
+            {key: 'username', required: true}
+        ];
+        const errors = Validator(formData, rules);
+        console.log(errors);
     };
     return (
         <Form value={formData} fields={fields}
